9a9733cbf1120236a6f01764d9ebaaac64ce30d9
Peter Lamberg <pgithub@pelam.fi>
2025-08-02 19:37:41 +0300

    First stab at media parsing

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 import re
-from src.chat_data import ChatData, Chat, ChatName, Message
+from src.chat_data import ChatData, Chat, ChatName, Message, MediaReference
 
 def parse_chat_txt(file_path):
     chat_data = ChatData()
@@ -11,12 +11,14 @@ def parse_chat_txt(file_path):
     year_regex = re.compile(r'\D(\d{4})\D')
     sender_regex = re.compile(r'(?<=\] ).*?(?=: )')
     content_regex = re.compile(r'(?<=: ).*')
+    media_regex = re.compile(r'<liite: (.*?)>')
 
     for line in lines:
         timestamp_match = timestamp_regex.search(line)
         year_match = year_regex.search(line)
         sender_match = sender_regex.search(line)
         content_match = content_regex.search(line)
+        media_match = media_regex.search(line)
 
         if timestamp_match and year_match and sender_match and content_match:
             timestamp = timestamp_match.group(1)
@@ -24,11 +26,16 @@ def parse_chat_txt(file_path):
             sender = sender_match.group(0)
             content = content_match.group(0)
 
+            media = None
+            if media_match:
+                media_file = media_match.group(1)
+                media = MediaReference(size=0, input_path=media_file, output_path=f"media/{media_file}")
+
             chat_name = ChatName(name="Default Chat")
             if chat_name not in chat_data.chats:
                 chat_data.chats[chat_name] = Chat(chat_name=chat_name)
 
-            message = Message(timestamp=timestamp, sender=sender, content=content, year=year)
+            message = Message(timestamp=timestamp, sender=sender, content=content, year=year, media=media, input_file=file_path)
             chat_data.chats[chat_name].messages.append(message)
 
     return chat_data
